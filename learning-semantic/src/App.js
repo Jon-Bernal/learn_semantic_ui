6f24107c0a60c617b1e463b2f4568a1ded1d6015
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-08-18 09:33:59 -0700

    added a button

--- learning-semantic/src/App.js
+++ learning-semantic/src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header>
       <h1 className="ui header">Business Name<i className="angle double right icon"></i></h1>
       </header>
+      <button class="ui button">
+        Follow
+      </button>
       <div className="ui left vertical inverted sidebar labeled icon menu visible">
         <a className="item">
           <i className="home icon"></i>
